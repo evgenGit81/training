3ae998811c7aebb9500b0d3fe1e80b2e37456b35
evgenGit81 <139344863+evgenGit81@users.noreply.github.com>
2024-03-18 09:43:18 +0000

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,11 @@
+name = 'Евгений'
+print('Имя: ', name)
+
+age = 43
+print('Возраст: ', age)
+
+new_age = age + 56
+print('Новый возраст:', new_age)
+
+is_student = True
+print('Is Student:', is_student)
