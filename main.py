fc4966aa63c5a18da250db14a269fb1fa4f4c71e
evgenGit81 <139344863+evgenGit81@users.noreply.github.com>
2024-04-03 18:51:34 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def test(a, b):
+    print("a =", a, '\n',  "b =", b)
+
+def test2(a, b, c):
+    print("a =", a, '\n',  "b =", b, '\n', "c =", c)
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    test(4, 5647)
+    test2(6, 9, 4)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
